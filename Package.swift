04ca2d08b047b9763df216db19ff8c61fc18602f
jillsong <jillsong@google.com>
2021-06-23 23:41:51 +0000

    Internal change
    PiperOrigin-RevId: 381141683

---
+++ Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version:5.3
+
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+  name: "GoogleMobileAds",
+  platforms: [.iOS(.v10)],
+  products: [
+    .library(
+      name: "GoogleMobileAds",
+      targets: ["GoogleMobileAdsTarget"]
+    )
+  ],
+  dependencies: [
+    .package(
+      name: "GoogleAppMeasurement",
+      url: "https://github.com/google/GoogleAppMeasurement.git",
+      "7.0.0"..<"9.0.0"
+    ),
+    .package(
+      name: "GoogleUserMessagingPlatform",
+      url: "https://github.com/googleads/GoogleUserMessagingPlatform.git",
+      "1.1.0"..<"3.0.0"
+    ),
+  ],
+  targets: [
+    .target(
+      name: "GoogleMobileAdsTarget",
+      dependencies: [
+        .target(name: "GoogleMobileAds"),
+        .product(name: "GoogleAppMeasurement", package: "GoogleAppMeasurement"),
+        .product(name: "GoogleUserMessagingPlatform", package: "GoogleUserMessagingPlatform"),
+      ],
+      path: "GoogleMobileAdsTarget"
+    ),
+    .binaryTarget(
+      name: "GoogleMobileAds",
+      // TODO(neycwang): Replace with the actual public URL.
+      url: "https://gad-repo.appspot.com/static/GoogleMobileAdsSdkIos-8.5.0-SPM.zip",
+      checksum: "3e207d319b3c44e13f2f22c229197aff626d4c518adcb3f08f0457f69688a4ae"
+    ),
+  ]
+)

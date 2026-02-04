164c65f0ad0f7dee87410374958df88db5726fcf
jillsong <jillsong@google.com>
2026-02-04 10:48:51 -0800

    Updating Swift Package Version Numbers to 13.0.0.
    PiperOrigin-RevId: 865482261

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 
 let package = Package(
   name: "GoogleMobileAds",
-  platforms: [.iOS(.v12)],
+  platforms: [.iOS(.v13)],
   products: [
     .library(
       name: "GoogleMobileAds",
